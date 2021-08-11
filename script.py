494949133ffc94fca540b24284a1f2837a1d2de1
root <root@localhost.localdomain>
2021-08-11 23:38:56 +0000

    Inclusão de comentarios e instruções. Nenhuma alteração realizada no código

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+###NeSte trecho do código especifico para as váriaveis que as mesmas vão receber range de IP como valor.
+
 import ipaddress
 interno1=ipaddress.ip_network('10.0.0.0/16')
 interno2=ipaddress.ip_network('10.50.0.0/16')
@@ -5,6 +7,8 @@ rangeVpn=ipaddress.ip_network('192.168.0.0/16')
 ipList=['241.223.148.36','26.66.77.16','60.142.8.92']
 
 
+
+##Neste trecho válido se o IP do arquivo pertence ao range especificados nas variaveis a cima
 
 def verificaRange(ip):
     ip=ipaddress.ip_address(ip)
@@ -18,12 +22,16 @@ def verificaRange(ip):
         return True
     return False
 
+## Este trecho valida as info do arquivo informado pelo usuario, faz a contagem de linhas do arquivo e cria as váriaveis abrindo seus respectivos arquivos para gravas as infos coletadas
+
 arquivoLog = input ("Qual o nome do arquivo de log?: ")
 arquivo=open(arquivoLog,'r')
 linhas=arquivo.readlines()
 porta22=open("porta22.txt","a")
 portaHttp=open("portaHttp.txt","a")
 semIp=open("semIp.txt","a")
+
+##Este é o trecho de processamento das informações isolando os src,dst,port e action.
 i=0
 for linha in linhas:
     if i==0:
@@ -39,9 +47,9 @@ for linha in linhas:
         semIp.write(linha + "\n")
         continue
     if(port=='22'):
-        porta22.write(linha + "\n" )
+        porta22.write(linha + "\n" )     ###Separa em um arquivo as info referente a porta 22
     if(port=='443' or port=='80'):
-        portaHttp.write(linha + "\n")
+        portaHttp.write(linha + "\n")    ###Separa em um arquivo as info referente a porta 80 e 443 
 
 
 
